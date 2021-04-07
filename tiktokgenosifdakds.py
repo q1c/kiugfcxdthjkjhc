29e369950fba3dd88d26238973193a663136e54e
q1c <81942345+q1c@users.noreply.github.com>
2021-04-06 22:17:35 -0500

    Create tiktokgenosifdakds.py

---
+++ tiktokgenosifdakds.py
@@ -0,0 +1,13 @@
+import random
+
+chars = "qwertyuiopasdfghjklzxcvbnm"
+
+while 1:
+ username_len = int(input("How long do you want your name to be? > "))
+ username_count = int(input("How many usernames do you want? > "))
+ for x in range(0,username_count)"
+  username = ""
+  for x in range(0,username_len):
+  username_char = random.choice(chars)
+  username = username + username_char
+  print("Available Username:. username)
